3a5b689e642c1442a8823faed1993f72f63cdb98
AndresDiazPinto <andresvis565@gmail.com>
2024-06-18 10:28:32 -0500

    Empaquetado
    Se realiza empaquetado para entrega a repositorio

--- src/config/global.js
+++ src/config/global.js
@@ -169,6 +169,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/722103_CF01_DU.zip',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
